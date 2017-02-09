457d6fc3589ac3501689bb3590f6d0a6cc927b4e
khAlexey <a.khobbikhozhin@yandex.ru>
2017-02-09 21:52:07 +0300

    test to ignore files

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,6 +11,7 @@ namespace Snake
         static void Main(string[] args)
         {
             Console.Write("Hello World!");
+            Console.Write("test v1");
             Console.ReadLine();
         }
     }

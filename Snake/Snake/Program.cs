bd0bae9464908f309e6083126825334c9cc0d587
khAlexey <a.khobbikhozhin@yandex.ru>
2017-02-11 16:32:39 +0300

    Добавил точку с координатами; Удалил строчку "Hello World";

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,7 +10,13 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+   
+            Console.SetCursorPosition( x1, y1 );
+            Console.Write( sym1 );
             Console.ReadLine();
         }
     }

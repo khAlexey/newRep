327af750af8f1b5bb0057fe3d603a8e9a6b692e1
khAlexey <a.khobbikhozhin@yandex.ru>
2017-02-16 22:12:24 +0300

    Заставили змейку двигаться; Написали несколько методов: 1) Описали, как змейка будет двигаться (добавление и удаление точки)

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -8,10 +8,11 @@ namespace Snake
 {
     class Snake : Figure
     {
-        
+        Direction direction;
 
-        public Snake(Point tail, int lenght, Direction direction)
+        public Snake(Point tail, int lenght, Direction _direction)
         {
+            direction = _direction;
             pList = new List<Point>();
 
             for (int i = 0; i < lenght; i++)
@@ -22,5 +23,23 @@ namespace Snake
             }
         }
 
+        internal void Move()
+        {
+            Point tail = pList.First();
+            pList.Remove(tail);
+            Point head = GetNextPoint();
+            pList.Add(head);
+
+            tail.Clear();
+            head.Draw();
+        }
+
+        public Point GetNextPoint()
+        {
+            Point head = pList.Last();
+            Point nextPoint = new Point( head );
+            nextPoint.Move(1, direction);
+            return nextPoint;
+        }
     }
 }
